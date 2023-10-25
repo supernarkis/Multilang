a752219a348ab853ebd1e7a0a1dfc8fb866ac9cf
Наркис Мирсайтов <mirsaitow@gmail.com>
2023-10-25 19:24:43 +0500

    add async, add telethon, move telebot, split code to main and workers

--- main.py
+++ main.py
@@ -1,123 +1,30 @@
-import subprocess
-import sys
-import shutil
-import os
-import threading
-import telebot
-from telethon import TelegramClient
-import asyncio
 import configparser
+from telethon import TelegramClient, events
+import workers
 
 config = configparser.ConfigParser()  # создаём объекта парсера
 config.read("config.ini")  # читаем конфиг
-
 
 tg_user_id = config["tg_user"]["tg_user_id"]
 api_id = config["tg_user"]["api_id"]
 api_hash = config["tg_user"]["api_hash"]
 phone = config["tg_user"]["phone"]
-token = config["Bot"]["token"]
 
-bot = telebot.TeleBot(token)
+# Создаем клиента Telegram
 client = TelegramClient('anon', api_id, api_hash)
 
-def process_video( message ):
-    video_link = message.text
-    temp_dir = f'./temp/{message.message_id}'
-    temp_video_dir = f"{temp_dir}/video"
-    temp_video = f"{temp_video_dir}/%(title)s.%(ext)s"
-    temp_audio = f"{temp_dir}/audio"
-    os.makedirs(temp_dir, exist_ok=True)
-    os.makedirs(temp_video_dir, exist_ok=True)
-    os.makedirs(temp_audio, exist_ok=True)
+@client.on(events.NewMessage(pattern='^/start$'))
+async def start(event):
+    await event.respond('Привет, отправь мне ссылку на видео.')
 
-    yd = subprocess.Popen(["yt-dlp", "-o", temp_video, video_link,"--format", "22"], shell=True, stdout=sys.stdout)
-    yd.communicate()
+@client.on(events.NewMessage)
+async def text_handler(event):
+    if event.is_private and event.text and 'youtu' in event.text:
+        video_url = event.text.replace('youtube.com/shorts/', 'youtube.com/watch?v=').replace('youtu.be/', 'youtube.com/watch?v=').replace('?si', '&').split('&')[0]
 
-    vot = subprocess.Popen(["vot-cli", video_link, "--output", temp_audio], shell=True, stdout=sys.stdout)
-    vot.communicate()
+        await workers.process_video(client, event, video_url)
 
-    temp_video_file_name = os.listdir(temp_video_dir)[0]
-    temp_video_file = temp_video_dir + "/" + temp_video_file_name
-    temp_audio_file = temp_audio + "/" + os.listdir(temp_audio)[0]
-
-    cmd = ['ffmpeg',
-            '-i', temp_video_file,
-            '-i',  temp_audio_file,
-            '-c:v', 'copy',
-            '-b:a', '128k',
-            '-filter_complex', '[0:a] volume=0.12 [original]; [original][1:a] amix=inputs=2:duration=longest [audio_out]',
-            '-map', '0:v',
-            '-map', '[audio_out]',
-            '-y', temp_video_file_name]
-
-    ff = subprocess.Popen(cmd, shell=True, stdout=sys.stdout)
-    ff.communicate()
-
-    shutil.rmtree(temp_dir)
-    return (temp_video_file_name)
-
-
-@bot.message_handler(commands=['start'])
-def start( message ):
-    bot.send_message(message.chat.id, f'i see you')
-
-
-@bot.message_handler(content_types=['text'])
-def text_handler( message ):
-    #если с ютуба
-    if "tube" in message.text:
-        threading.Thread(target=video_translator, args=(bot, message)).start()
-
-
-def video_translator( bot, message ):
-        # заказать перевод и переслать заказчику
-        filename = process_video(message)
-        send_file(filename, message.from_user.id)
-
-
-@bot.message_handler(content_types=['document', 'video'])
-def file_handler( message ):
-    # Файлы от админа с caption
-    if message.from_user.id == tg_user_id and message.caption:
-        # Создаем новый поток для пересылки
-        threading.Thread(target=redirect_to_customer, args=(bot, message)).start()
-
-
-def redirect_to_customer(bot, message ):
-    #Пересылаем файл по id укзанному в caption
-    try:
-        bot.send_video(message.caption, str(message.document.file_id))
-    except:
-        print("Не удалось отправить файл ", message.document.file_name, " в чат c id ", message.caption)
-
-
-def send_file( filename, id ):
-    try:
-        if os.path.getsize(f"./{filename}") <= 49*1024*1024:
-            with open(f'./{filename}', "rb") as file:
-                bot.send_video(id, file)
-        else:
-
-            async def tasker (filename, id):
-                task = asyncio.create_task(send_file_fromclient(filename, id))
-                await task
-
-            client.connect()
-            asyncio.run(tasker(filename, id))
-
-    except():
-        print("Error occurred on function main.py - send_file")
-    finally:
-        os.remove(f"./{filename}")
-    return
-
-
-async def send_file_fromclient( filename, id ):
-    await client.send_file('Multilang_bot', f"./{filename}", caption=str(id))
-
-
-
-print("Run!")
-bot.polling()
-
+if __name__ == '__main__':
+    client.start()
+    print("Бот запущен.")
+    client.run_until_disconnected()
---
+++ workers.py
@@ -0,0 +1,77 @@
+import asyncio
+import os
+import shutil
+import subprocess
+import sys
+
+
+async def process_video(client, event, video_url):
+    temp_dir = f'temp/{event.id}'
+    temp_video_dir = f'{temp_dir}/video'
+    temp_audio_dir = f'{temp_dir}/audio'
+    video_file = f"{temp_video_dir}/%(title)s.%(ext)s"
+
+    os.makedirs(temp_dir, exist_ok=True)
+    os.makedirs(temp_video_dir, exist_ok=True)
+    os.makedirs(temp_audio_dir, exist_ok=True)
+
+    await download_video(event, video_url, video_file)
+    await download_audio(video_url, temp_audio_dir)
+    video_file = f'{temp_video_dir}/{os.listdir(temp_video_dir)[0]}'
+    audio_file = f'{temp_audio_dir}/{os.listdir(temp_audio_dir)[0]}'
+    result_file = f'{temp_dir}/{os.listdir(temp_video_dir)[0]}'
+    try:
+        if video_file and audio_file:
+            await edit_video(video_file, audio_file, result_file)
+            if result_file:
+                await send_large_file(client, event, result_file)
+    finally:
+        shutil.rmtree(temp_dir)
+
+async def download_video(event, video_url, video_file):
+    try:
+        process = await asyncio.create_subprocess_exec('yt-dlp', video_url, '-o', video_file, "-f", "22")
+        await process.wait()
+    except Exception as e:
+        await event.respond(f'Не удалось получить видео по ссылке')
+        print(f'Ошибка в download_video: {str(e)}')
+
+
+async def download_audio(video_url, temp_audio_dir):
+    try:
+        process = subprocess.Popen(['vot-cli', video_url, '--output', temp_audio_dir], shell=True, stdout=sys.stdout)
+        process.communicate()
+        print('Переведенный аудио файл скачан')
+    except Exception as e:
+        print(f'Ошибка при скачивании переведенного аудио: {str(e)}')
+
+
+async def edit_video(video_file, audio_file, result_file):
+    try:
+        process = await asyncio.create_subprocess_exec(
+            'ffmpeg',
+            '-i', video_file,
+            '-i', audio_file,
+            '-c:v', 'copy',
+            '-b:a', '128k',
+            '-filter_complex', '[0:a] volume=0.12 [original]; [original][1:a] amix=inputs=2:duration=longest [audio_out]',
+            '-map', '0:v',
+            '-map', '[audio_out]',
+            '-y', result_file
+        )
+        await process.wait()
+        print('Видео собрано')
+    except Exception as e:
+        print(f'Ошибка при пересборке видео: {str(e)}')
+
+async def send_large_file(client, event, file_path):
+    try:
+        await client.send_file(event.chat_id, os.path.abspath(file_path), mime_type='video/mp4')
+    except Exception as e:
+        await event.respond(f'Не узалось загрузить переведенное видео в телеграм: {str(e)}')
+        print(f'Произошла ошибка в send_large_file: {str(e)}')
+
+
+if __name__ == '__main__':
+    client.start()
+    client.run_until_disconnected()
